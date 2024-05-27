9504cc7ef967b3f675fc3ece477cbe1924b9a558
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2024-05-26 22:12:31 -0500

    Se listan las listas que se tienen de ejemplo en en Context

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,6 +5,28 @@ import PropTypes from 'prop-types';
 
 export const DoingTaskContext = createContext();
 
+// Default Lists
+const defaultLists = [
+   {
+      name: 'Home',
+      description: 'Home things',
+      nTasks: 0,
+      nTasksCompleted: 0,
+   },
+   {
+      name: 'Office',
+      description: 'Office things',
+      nTasks: 0,
+      nTasksCompleted: 0,
+   },
+   {
+      name: 'Hobbies',
+      description: 'Hobbies things',
+      nTasks: 0,
+      nTasksCompleted: 0,
+   },
+];
+
 export const DoingTaskProvider = ({ children }) => {
    // Location
    const location = useLocation();
@@ -17,6 +39,9 @@ export const DoingTaskProvider = ({ children }) => {
 
    // Navbar title
    const [pageTitle, setPageTitle] = useState('Home');
+
+   // Lists
+   const [lists, setLists] = useState(defaultLists);
 
    // Update page title on navigate
    useEffect(() => {
@@ -49,6 +74,8 @@ export const DoingTaskProvider = ({ children }) => {
             setSearchValue,
             pageTitle,
             setPageTitle,
+            lists,
+            setLists,
          }}
       >
          {children}
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,3 +1,22 @@
-export const List = () => {
-   return <h1>List</h1>;
+// Props Types
+import PropTypes from 'prop-types';
+
+export const List = ({ name, description, nTasks, nTasksCompleted }) => {
+   return (
+      <li className='border border-slate-300 rounded-lg mb-3'>
+         <h2>{name}</h2>
+         <div>
+            <p>{description}</p>
+            <p>Tasks: {nTasks}</p>
+            <p>Completed Tasks: {nTasksCompleted}</p>
+         </div>
+      </li>
+   );
+};
+
+List.propTypes = {
+   name: PropTypes.string,
+   description: PropTypes.string,
+   nTasks: PropTypes.number,
+   nTasksCompleted: PropTypes.number,
 };
